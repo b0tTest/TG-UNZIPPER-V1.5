188aa53fdb45f471834f7c44708f050ed4bf794a
Esther-Lopez <66007532+Esther-Lopez@users.noreply.github.com>
2021-10-07 12:26:59 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,9 +1,10 @@
 echo "
-||| Unzipper Bot |||
+||| TG_UnzipperBot |||
 
 Copyright (c) 2021 Itz-fork
+--> Fork from itz-fork
 
---> Join @NexaBotsUpdates
---> Follow Itz-fork on Github
+--> Join @MyTestBotZ
+
 "
 python3 -m unzipper
